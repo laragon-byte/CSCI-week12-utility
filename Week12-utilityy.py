8404a53dde0bb1e0fb9072a2fbd62b2bd76f414c
laragon-byte <57676218+laragon-byte@users.noreply.github.com>
2019-11-18 18:20:12 -0700

    Add files via upload

--- Week12-utilityy.py
+++ Week12-utilityy.py
@@ -2,5 +2,18 @@
 #Leonardo Aragon
 #CSCI 102-Section C
 #Week 11-Part B
+
 def PrintOutput(string):
     print('OUTPUT',string)
+
+def LoadFile(file):
+    read_file=open(file,"r")
+    unedited=read_file.read()
+    read_file.close()
+    l = []
+    for line in in_file:
+        l.append(line.split(','))
+    print('OUTPUT', l)
+
+
+    
